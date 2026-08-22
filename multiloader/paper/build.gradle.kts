33f587ea8fa6f407952d2ee36780f38d72751d32
Nodedistro <admin@nodedistro.com>
2026-08-22 10:33:54 -0600

    Fix Paper 26.1.2 dependency version

--- multiloader/paper/build.gradle.kts
+++ multiloader/paper/build.gradle.kts
@@ -5,11 +5,20 @@ version = "2.0.0"
 
 repositories {
     mavenCentral()
-    maven { name = "papermc"; url = uri("https://repo.papermc.io/repository/maven-public/") }
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
-dependencies { compileOnly("io.papermc.paper:paper-api:26.1.2-R0.1-SNAPSHOT") }
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+}
 
-java { toolchain.languageVersion.set(JavaLanguageVersion.of(25)) }
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
 
-tasks.jar { archiveBaseName.set("TNT-Hopper-Protect-Paper") }
+tasks.jar {
+    archiveBaseName.set("TNT-Hopper-Protect-Paper")
+}
